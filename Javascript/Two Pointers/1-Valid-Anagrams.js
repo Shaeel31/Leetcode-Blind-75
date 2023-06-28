35bd49136fcf07050b3ce3fba252bc1d8fc56a68
Shaeel786 <shaeel678@gmail.com>
2023-06-28 11:33:02 +0500

    Valid Anagrams Shorter Soloution

--- Javascript/Two Pointers/1-Valid-Anagrams.js
+++ Javascript/Two Pointers/1-Valid-Anagrams.js
@@ -1,10 +1,32 @@
+
+
+// Soloution 1 - With 0(N) Time Complexity
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isPalindrome = function (s) {
     s = s.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "").replace(/ /g, '');
-    let c = s.split('').reverse().join('')
-    if (c == s) return true;
-    return false;
+    s = s.split('');
+    let l = 0;
+    let r = s.length - 1
+    while (l < r) {
+        if (s[l] == s[r]) {
+            l++;
+            r--
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 };
+
+// Soloution 2 - Shorter Version
+
+var isPalindrome = function (string) {
+    string = string.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "").replace(/ /g, '')
+    return(( string) === string.split('').reverse().join(''))
+};
+
