c71b56ffd57fab932da41f8cb433f2c50fbfb869
Shaeel786 <shaeel678@gmail.com>
2023-06-29 19:25:22 +0500

    2nd Soloution

--- Javascript/Arrays & HashMaps/4-Product-of-Array-Except-Self.js
+++ Javascript/Arrays & HashMaps/4-Product-of-Array-Except-Self.js
@@ -20,7 +20,33 @@ return resultant;
 productExceptSelf([1,2,3,4])
 
 
+// 2nd Soloution with O(N) time complexity
+
+var productExceptSelf = function (nums) {
+   let left_prod = []
+   let right_prod = []
+   let output_arr = []
+   left_prod[0] = 1
+   right_prod[nums.length - 1] = 1
+
+   for (let i = 1; i < nums.length; i++) {
+      left_prod[i] = nums[i - 1] * left_prod[i - 1]
+   }
+
+   for (let j = nums.length - 2; j >= 0; j--) {
+      right_prod[j] = nums[j + 1] * right_prod[j + 1]
+   }
+   for (let k = 0; k < nums.length; k++) {
+      output_arr[k] = left_prod[k] * right_prod[k]
+   }
+   return output_arr;
+}
+
+productExceptSelf([1, 2, 3, 4])
+
+
 // Time Complexity O(N)
 // Space Complexity o(N)
+
 
 
