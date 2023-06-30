beccd7074b65f2e75b05b0edd4ce0c26915da01f
Shaeel786 <shaeel678@gmail.com>
2023-06-30 16:07:49 +0500

    Leetcode medium longest substring without repeating characters

---
+++ Javascript/Sliding Window/1-Longest-substring-without-repeating-characters.js
@@ -0,0 +1,26 @@
+var lengthOfLongestSubstring = function (s) {
+    s = s.split('')
+    let max = 0
+    let set = new Set();
+    let l_pointer = 0
+    let r_pointer = 0
+
+    while (r_pointer < s.length) {
+        if (!set.has(s[r_pointer])) {
+            set.add(s[r_pointer]);
+            r_pointer++
+            max =Math.max(max , set.size );
+        }
+        else {
+            set.delete(s[l_pointer])
+            l_pointer++
+        }
+    }
+    return max;
+
+};
+lengthOfLongestSubstring("abcabcbb")
+
+
+// Time Complexity O(N)
+// Space Complexity O(N)
