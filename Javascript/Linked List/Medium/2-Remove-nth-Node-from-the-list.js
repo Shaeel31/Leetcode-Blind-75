606cc70b4848b4a9478914a612c6bc085c3b47f9
Shaeel786 <shaeel678@gmail.com>
2023-07-22 00:31:01 +0500

    Leetcode medium Remove Nth Node From the List

---
+++ Javascript/Linked List/Medium/2-Remove-nth-Node-from-the-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    let dummy_head = new ListNode(0 , head)
+       let left = dummy_head
+       let right = head
+   
+       while (n > 0 && right) {
+           right = right.next
+           n--
+       }
+       while (right) {
+           left = left.next;
+           right = right.next;
+       }
+       left.next = left.next.next;
+   
+       return dummy_head.next
+   }
