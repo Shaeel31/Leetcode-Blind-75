7330b69550017f65b4e5c5b21b6c875ddb7a2378
Shaeel786 <shaeel678@gmail.com>
2023-07-26 23:37:16 +0500

    Leetocode easy subtree of another tree

---
+++ Javascript/Trees/Easy/4-Subtree-of-another-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+    if (!subRoot) return true;
+    if (!root) return false;
+    if(isSameTree(root,subRoot)) return true;
+    return isSubtree(root.left , subRoot) || isSubtree(root.right , subRoot);
+};
+
+var isSameTree = function (s, r) {
+    if (!s && !r) return true;
+    if ((s && r) && (s.val == r.val)) {
+        return (isSameTree(s.left, r.left) && isSameTree(s.right, r.right))
+    }
+    return false;
+}
+
