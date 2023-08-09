fb192fa02d0c0911791dff377a0239d31d29ce8b
Shaeel786 <shaeel678@gmail.com>
2023-08-09 12:18:56 +0500

    Leetcode Medium [Construct Trees from preorder & Inorder traversals]- [Commensts added explaining left & right sub trees structure]

--- Javascript/Trees/Medium/5-Contruct-binary-tree-from-preorder-&-inorder-traversals.js
+++ Javascript/Trees/Medium/5-Contruct-binary-tree-from-preorder-&-inorder-traversals.js
@@ -25,13 +25,20 @@ var buildTree = function(preorder, inorder) {
 
        
        // Use the recursive approach and keep on calling the function to make left and right sub trees
+       //In the left sub tree for preorder we start from 1 as 0 index is the root node and 
+       // slice up unitil mid elelement in preorder array as we have to slice up until index of the root node in inorder array.  
        root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+
+       // Same for the right sub tree for preorder array we start at the mid up until the end and 
+       // for in order array we start from the mid  up until the end as the positions of the elements in preorder
+       // and in order arrays are already sorted
        root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
 
        // The root is returned at the end as we have to return the root of newly created tree
        return root;
    };
 
-   buildTree()
+   buildTree([10,20,40,50,30,60] , [40,20,50,10,60,30])
 
    //Complexity : O(N)
+
