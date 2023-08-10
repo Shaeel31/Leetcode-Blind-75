04c65fe1a980caa090ee75240a2e775d6e42a4bf
Shaeel786 <shaeel678@gmail.com>
2023-08-10 23:40:13 +0500

    [Heaps/Priority-Queues] - Kth Largest Element In Array - [Folder Segragation]

--- Javascript/Heaps-PriorityQueues/Easy/1-Last-stone-weight.js
+++ Javascript/Heaps-PriorityQueues/Easy/1-Last-stone-weight.js
@@ -16,3 +16,9 @@ var lastStoneWeight = function(stones) {
        return queue.size() === 0 ? 0 : queue.front().element   
    };
 lastStoneWeight([2,7,4,1,8,1])
+
+// Important Notes :  We make a max priority Queue and add all the stones in it 
+// We get the 2 maximum stones and smash them if they are equal, both of them are destroyed
+// If not than there difference is added to the priority queue.
+// We keep the track of the elements present in the queue if there is only single element 
+// present in  the queue or the queue is empty than we return the el or 0 in other case.
---
+++ Javascript/Heaps-PriorityQueues/Medium/1-Kth-largest-element-in-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    let queue = new MaxPriorityQueue()
+    let element;
+    for(let el of nums)  queue.enqueue(el);
+    while(k !== 0){
+         element = queue.dequeue().element;
+         k--
+    }
+    return element;
+};
