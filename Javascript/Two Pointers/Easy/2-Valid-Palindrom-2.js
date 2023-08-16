dd9df19ab26e46bb0b68324678e319029b2d7696
Shaeel786 <shaeel678@gmail.com>
2023-08-16 23:38:03 +0500

    [Two Pointers] - Valid Palindrom 2 - [Soloution]

---
+++ Javascript/Two Pointers/Easy/2-Valid-Palindrom-2.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let left = 0;
+      let right = s.length-1;
+      while(left<right){
+          if(s.charAt(left)==s.charAt(right)){
+              left++;
+              right--;
+          }
+          if(s.charAt(left)!=s.charAt(right)){
+              return checkPalindrome(s,left+1,right) || checkPalindrome(s,left,right-1);
+          }
+      }
+      return true;
+  };
+  
+  const checkPalindrome = (s,i,j) => {
+      let left = i;
+      let right = j;
+      while(left<right){
+          if(s.charAt(left)==s.charAt(right)){
+              left++;
+              right--;
+          }
+          if(s.charAt(left)!=s.charAt(right)){
+              return false;
+          }
+      }
+      return true;
+  };
