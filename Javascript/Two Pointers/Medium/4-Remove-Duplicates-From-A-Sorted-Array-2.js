49d84694664d16af0d998d5639a97b8e96601ec6
Shaeel786 <shaeel678@gmail.com>
2023-08-20 14:44:37 +0500

    [Two Pointers] - Remove-Duplicates-From-A-Sorted-Array-2 - [Soloution]

---
+++ Javascript/Two Pointers/Medium/4-Remove-Duplicates-From-A-Sorted-Array-2.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let i = 1;
+    let c = 1
+
+    while (i < nums.length) {
+        if (nums[i - 1] == nums[i] && c < 2) {
+            c++
+            i++
+        }
+        else if (nums[i - 1] == nums[i] && c >= 2) {
+            nums.splice(i, 1);
+        }
+        else if (nums[i] !== nums[i - 1]){
+            c = 1;
+            i++
+        }
+    }
+    return nums.length;
+};
