01eb29653bd93571f235962165ca51a4d1f48979
Shaeel786 <shaeel678@gmail.com>
2023-08-27 12:36:43 +0500

    [Two-Pointers]- 3 Sum - [Comments Added]

--- Javascript/Two Pointers/Medium/2-Three-Sum.js
+++ Javascript/Two Pointers/Medium/2-Three-Sum.js
@@ -1,42 +1,57 @@
-
-
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * @param {number[]} nums - The input array of numbers
+ * @return {number[][]} - An array containing arrays of three numbers that sum up to zero
  */
 var threeSum = function (nums) {
+    // Sort the input array in ascending order
     nums = nums.sort((a, b) => a - b);
-    let res = []
+    
+    // Initialize an array to store the resulting triplets
+    let res = [];
+    
+    // Iterate through each element in the sorted array
     for (let i = 0; i < nums.length; i++) {
+        // Current element under consideration
         let c = nums[i];
-        if (i > 0 && (c == nums[i - 1])) continue;
-        let l = i + 1;
-        let r = nums.length - 1
-        while ((l < r)) {
-            let sum = c + nums[l] + nums[r]
-            if (sum == 0) {
-                res.push([nums[i], nums[l], nums[r]]);
-                l += 1;
-                r -= 1
-                while (nums[l] == nums[l - 1] && l < r) {
-                    l += 1;
+        
+        // Skip duplicate elements by checking against the previous element
+        if (i > 0 && c === nums[i - 1]) continue;
+        
+        // Initialize pointers to find the other two elements for the triplet
+        let [l, r] = [i + 1, nums.length - 1];
+        
+        // Loop to find the other two elements
+        while (l < r) {
+            // Calculate the sum of the three elements
+            let s = c + nums[l] + nums[r];
+            
+            if (s === 0) {
+                // If the sum is zero, a valid triplet is found
+                
+                // Add the triplet to the result array
+                res.push([c, nums[l], nums[r]]);
+                
+                // Increment left pointer and decrement right pointer
+                l++;
+                r--;
+                
+                // Skip duplicates by advancing the left pointer
+                while (l < r && nums[l - 1] === nums[l]) {
+                    l++;
                 }
+            } else if (s > 0) {
+                // If the sum is greater than zero, decrement the right pointer
+                r--;
+            } else if (s < 0) {
+                // If the sum is less than zero, increment the left pointer
+                l++;
             }
-            else if (sum > 0) {
-                r -= 1;
-            }
-            else if (sum < 0) {
-                l += 1;
-            }
-
         }
     }
+    
+    // Return the array containing all the unique triplets
     return res;
-}
+};
 
-threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]);
-
-
-// Time Complexity O(n2)
-
-
+// Test the function with an example input
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
