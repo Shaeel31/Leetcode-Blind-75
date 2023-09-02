a05d824f22cadb3b749dd19ede739b5784528cd5
Shaeel786 <shaeel678@gmail.com>
2023-09-03 00:28:52 +0500

    [Two Pointers] - Minimum boats to accomodate people - [Complexity Analysis]

--- Javascript/Two Pointers/Medium/5-Boats-to-save-people.js
+++ Javascript/Two Pointers/Medium/5-Boats-to-save-people.js
@@ -21,3 +21,21 @@ var numRescueBoats = function(people, limit) {
     // Step 3: Return the total number of boats needed for the rescue operation.
     return boats;
 };
+
+// COMPLEXITY ANALYSIS
+
+/* 
+The time complexity of the above code is O(n log n), where 'n' is the number of people in the `people` array. This complexity arises mainly from the sorting step.
+
+Here's a breakdown of the complexity:
+
+1. Sorting: The `people` array is sorted in ascending order based on their weights using the `sort` method,
+ which has a time complexity of O(n log n) for typical sorting algorithms. This is the dominant factor in the overall time complexity.
+2. Loop: The `while` loop iterates through the sorted `people` array once, comparing and updating pointers and counting boats. This loop's time complexity is O(n).
+Asymptotically, O(n log n) dominates the time complexity, so the overall time complexity of the code is O(n log n).
+
+The space complexity of the code is O(1) because it uses a constant amount of additional memory for variables like `boats`,
+ `left`, and `right`, regardless of the input size. The sorting operation is performed 
+ in-place on the original array, so it doesn't incur any significant additional space complexity.
+
+ */
