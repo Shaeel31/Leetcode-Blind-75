6b29a0642e031b006bedc7d52390c9cfc17b0cc4
Shaeel786 <shaeel678@gmail.com>
2023-09-03 00:26:41 +0500

    [Two Pointers] - Minimum boats to accomodate people - [Soloution]

--- Javascript/Two Pointers/Medium/5-Boats-to-save-people.js
+++ Javascript/Two Pointers/Medium/5-Boats-to-save-people.js
@@ -1,21 +1,23 @@
-/**
- * @param {number[]} people
- * @param {number} limit
- * @return {number}
- */
-var numRescueBoats = function (people, limit) {
-    let a = 0;
-    let b = people.length - 1;
-    let boats = 0
-    people = people.sort((a, b) => a - b)
-    while (a <= b) {
-        debugger
-        if (people[a] + people[b] <= limit) {
-            a++
+// Define a function to calculate the minimum number of boats needed to rescue people.
+var numRescueBoats = function(people, limit) {
+    // Step 1: Sort the 'people' array in ascending order based on their weights.
+    people.sort((a, b) => a - b);
+    
+    // Initialize variables
+    let boats = 0;    // To count the number of boats needed
+    let left = 0;     // Pointer for the lightest person
+    let right = people.length - 1; // Pointer for the heaviest person
+
+    // Step 2: Loop through the 'people' array to pair individuals and calculate boats needed.
+    while (left <= right) {
+        // Check if the sum of weights of 'left' and 'right' persons is within the 'limit'.
+        if (people[left] + people[right] <= limit) {
+            left++; // Move to the next lightest person.
         }
-            b--
-            boats++
+        right--;   // Move to the next heaviest person.
+        boats++;   // Increment the boat count for each pairing.
     }
+
+    // Step 3: Return the total number of boats needed for the rescue operation.
     return boats;
 };
-numRescueBoats([3,5,3,4],3)
