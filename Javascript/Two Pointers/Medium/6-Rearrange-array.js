9d3cce7c562fafa2493c5c54067abd60435383de
Shaeel786 <shaeel678@gmail.com>
2023-09-06 22:41:12 +0500

    [Two Pointers] - Rearrange-Array - [2nd Soloution]

--- Javascript/Two Pointers/Medium/6-Rearrange-array.js
+++ Javascript/Two Pointers/Medium/6-Rearrange-array.js
@@ -3,6 +3,7 @@
  * @return {number[]}
  */
 
+// 1st Soloution 
 
 var rearrangeArray = function (nums) {
     let b = 1;
@@ -23,4 +24,39 @@ var rearrangeArray = function (nums) {
 rearrangeArray([10,7,5,4,3])
 
 //Time Complexity O(N^2)
+//Space Complexity O(N)
+
+
+// 2nd Soloution 
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function (nums) {
+    nums.sort((a, b) => a - b); // Sort the array in ascending order
+    const n = nums.length;
+    const result = new Array(n);
+
+    let left = 0;
+    let right = n - 1;
+    let index = 0;
+
+    while (left <= right) {
+        if (index % 2 === 0) {
+            // Place the larger numbers in the result array for even indices
+            result[index] = nums[right];
+            right--;
+        } else {
+            // Place the smaller numbers in the result array for odd indices
+            result[index] = nums[left];
+            left++;
+        }
+        index++;
+    }
+
+    return result;
+};
+
+//Time Complexity O(N)
 //Space Complexity O(N)
