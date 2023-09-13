01fa471d66f8002190e55c4763cadb6fa2dfd34b
Shaeel786 <shaeel678@gmail.com>
2023-09-13 23:19:36 +0500

    Two Pointers Merge Alternatives

---
+++ Javascript/Two Pointers/Easy/5-Merge-Alternatives.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+    let r = 0;
+    let greater = Math.max(word1.length, word2.length)
+    let el = ""
+    while (r < greater) {
+        let elw1 = typeof word1[r] === 'undefined' ? '' : word1[r];
+        let elw2 = typeof word2[r] === 'undefined' ? '' : word2[r];
+        el += elw1;
+        el += elw2
+        r++
+    }
+    
+        return el;
+}
