6bee49270e023acd3e5b55217bff7d1ddc0be794
Shaeel678 <shaeel678@gmail.com>
2023-11-12 15:04:25 +0500

    white spaces removed

--- Phase 2/Arrays & HashMaps/3 - Group-anagrams.js
+++ Phase 2/Arrays & HashMaps/3 - Group-anagrams.js
@@ -8,7 +8,6 @@ var groupAnagrams = function (strs) {
     for (let i = 0; i < strs.length; i++) {
         let cur = strs[i];
         let sorted = strs[i].split('').sort().join('');
-
         if (!dict[sorted]) {
             dict[sorted] = [cur];
         }
