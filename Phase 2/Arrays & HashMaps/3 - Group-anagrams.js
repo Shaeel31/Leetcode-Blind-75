b11a26808445db72712b91260fc821218c7fdbf0
Shaeel678 <shaeel678@gmail.com>
2023-11-12 14:31:46 +0500

    Arrays & Hashmaps - Group-anagrams

---
+++ Phase 2/Arrays & HashMaps/3 - Group-anagrams.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    let dict = {}
+
+    for (let i = 0; i < strs.length; i++) {
+        let cur = strs[i];
+        let sorted = strs[i].split('').sort().join('');
+
+        if (!dict[sorted]) {
+            dict[sorted] = [cur];
+        }
+        else {
+            dict[sorted].push(cur);
+        }
+    }
+
+    return Object.values(dict);
+
+};
