de6ec1a67b854b3121b09ee5c3a604996c9da7e4
Shaeel678 <shaeel678@gmail.com>
2024-01-13 22:56:12 +0500

    Minimum Number of Steps to Make Two Strings Anagram

--- Other Problems/Medium/Minimum-Number-of-Steps-to-Make-Two-Strings-Anagram.js
+++ Other Problems/Medium/Minimum-Number-of-Steps-to-Make-Two-Strings-Anagram.js
@@ -35,3 +35,28 @@ var minSteps = function (s, t) {
  return steps ;
 };
 
+
+// Better approach with optimised soloution
+
+
+var minSteps = function (s, t) {
+    if (s.length !== t.length) return -1; // strings must be of equal length
+    let sHashMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        sHashMap[s[i]] = (sHashMap[s[i]] || 0) + 1;
+    }
+
+    let steps = 0;
+
+    for (let i = 0; i < t.length; i++) {
+        if (sHashMap[t[i]] !== undefined && sHashMap[t[i]] > 0) {
+            sHashMap[t[i]]--;
+        } else {
+            steps++;
+        }
+    }
+
+    return steps;
+};
+
