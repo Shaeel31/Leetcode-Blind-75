c702bc5e477174e1926807510a09e51f33f5c402
Shaeel678 <shaeel678@gmail.com>
2024-01-26 00:15:05 +0500

    896 - Is-Monotonic

---
+++ Other Problems/Easy/896-Is-Monotonic.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function (nums) {
+    let isIncreasing = false;
+    let isDecreasing = false;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j - 1] == nums[j]) {
+            continue;
+        }
+        if (nums[j - 1] > nums[j]) {
+            isDecreasing = true
+            break;
+        }
+        else if (nums[j - 1] < nums[j]) {
+            isIncreasing = true;
+            
+        }
+    }
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (nums[i - 1] == nums[i]) {
+            continue;
+        }
+        if ((nums[i - 1] < nums[i]) && isDecreasing) {
+            return false;
+        }
+        if ((nums[i - 1] > nums[i]) && isIncreasing) {
+            return false;
+        }
+    }
+    return true;
+};
